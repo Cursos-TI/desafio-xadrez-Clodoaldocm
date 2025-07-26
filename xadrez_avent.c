bc942207c38e532cc390d87c5610348b25e3e580
Clodoaldo <53956052+Clodoaldocm@users.noreply.github.com>
2025-07-25 21:55:32 -0300

    Movimento Cavalo

---
+++ xadrez_avent.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+	int torre = 5;
+	int bispo = 5;
+	int rainha = 8;
+
+	//movimento da torre
+	printf("Movimento da Torre:\n");
+	printf("T");
+	for(int i=0;i<torre;i++){
+		printf(" >> direita");
+	}
+	
+	//movimento do Bispo
+	
+	printf("\nMovimento do Bispo:\n");
+	int mbispo = bispo;
+	printf("B");
+	while(mbispo>1){
+		printf(" >> Cima >>");
+		printf("Direita");
+		mbispo--;
+	}
+	
+	//movimento da rainha
+	printf("\nMovimento da Rainha\n");
+	printf("R");
+	do{
+		printf("<<Esquerda");
+		rainha--;
+	}while(rainha >0);
+	printf("\n");
+
+	//movimento do cavalo
+	printf("\nMovimento do Cavalo:\n");
+	int ncasaAbaixo=2;
+	int ncasaEsq =1;
+	do{
+		for(int i=0;i<ncasaAbaixo;i++){
+		printf("Abaixo\n");
+		
+		}
+		printf("Esquerda\n");
+		ncasaEsq--;
+	
+	}while(ncasaEsq>0);
+
+	return 0;
+}
